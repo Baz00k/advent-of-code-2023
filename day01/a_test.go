fd930832cd25967e7fba5a1bc666835ac68bb2c3
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-12-07 17:52:32 +0100

    chore: rename test input

---
+++ day01/a_test.go
@@ -0,0 +1,25 @@
+package day01
+
+import (
+	"testing"
+
+	"github.com/Baz00k/advent-of-code-2023/helpers"
+)
+
+func TestSolvePartA(t *testing.T) {
+	input, err := helpers.ReadLines("test_a.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	result := SolvePartA(input)
+
+	if result != 142 {
+		t.Errorf("Expected 142, got %d", result)
+
+		return
+	}
+
+	t.Log("Success")
+}
