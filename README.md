d5899e9e954f5fa1fc1864d110e0c074f90824e2
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-12-10 01:48:38 +0100

    feat: add command line flag to specify day to solve

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 My solutions to the [Advent of Code 2023](https://adventofcode.com/2023) puzzles.
 Please, bear in mind that these are first lines of GO I've ever written, so the solutions are probably not the most elegant ones.
+
+## Usage
+
+To run the solution for a given day, just run the following command:
+
+```bash
+go run . -day <day>
+```
--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/Baz00k/advent-of-code-2023/day01"
+	"github.com/Baz00k/advent-of-code-2023/day02"
 )
 
 func main() {
-	day01.Solve()	
+    day := flag.Int("day", 0, "Specify the day to solve.")
+    d := flag.Int("d", 0, "Specify the day to solve.")
+    flag.Parse()
+
+    if *day == 0 && *d == 0 {
+        fmt.Println("Please provide a day to solve.")
+        return
+    }
+
+    dayToSolve := *day
+    if *d != 0 {
+        dayToSolve = *d
+    }
+
+    switch dayToSolve {
+    case 1:
+        day01.Solve()
+    case 2:
+        day02.Solve()
+    default:
+        fmt.Println("Invalid day provided.")
+    }
 }
