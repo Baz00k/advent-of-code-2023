8ac5c9aa81e5c8b10da522d947e51e0e355d9bc5
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-12-11 02:19:19 +0100

    feat: add ReadChars function to read a file to an array of characters

--- helpers/input.go
+++ helpers/input.go
@@ -23,3 +23,22 @@ func ReadLines(path string) ([]string, error) {
 	}
 	return lines, nil
 }
+
+// Read a file to an array of characters
+func ReadChars(path string) ([][]byte, error) {
+	if path == "" {
+		path = "./input.txt"
+	}
+
+	var chars [][]byte
+	file, err := os.Open(path)
+	if err != nil {
+		return chars, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		chars = append(chars, []byte(scanner.Text()))
+	}
+	return chars, nil
+}
